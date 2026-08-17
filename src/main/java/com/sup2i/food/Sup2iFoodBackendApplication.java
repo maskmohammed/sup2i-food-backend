6dadb2d97c587ee091f93133830fd4c05daecbfc
unknown <malliendd8@gmail.com>
2026-08-17 23:41:36 +0100

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/sup2i/food/Sup2iFoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sup2i.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sup2iFoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sup2iFoodBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sup2i/food/Sup2iFoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sup2i.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sup2iFoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
